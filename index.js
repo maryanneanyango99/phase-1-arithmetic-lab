54ec0dde1163a8ac0333e4bbcba2ceb22ae15dc4
Maryanne Anyango <mayannanneanyango98@gmail.com>
2022-06-10 17:54:48 +0300

    files

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+// random
+const random = Math.floor(Math.random() * 10) + 1;
+
+// modulus
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+
+
+// max
+const max = Math.max(1, 2, 3, 4, 5 , 6, 7, 8, 9,20);
